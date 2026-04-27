fdc1feb780dd55983016cdba52cc4f818572861d
mcondesso <mcondesso@mail.com>
2026-04-27 11:51:38 +0200

    Structure code and add animal data to html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,29 +1,58 @@
 import json
 
 
+def load_file(file_path):
+    """ Loads a file """
+    with open(file_path, "r", encoding="utf-8") as handle:
+        return handle.read()
+
+
+def write_file(file_path, data):
+    """ Writes data to a file """
+    with open(file_path, "w", encoding="utf-8") as handle:
+        handle.write(data)
+
+
 def load_data(file_path):
     """ Loads a JSON file """
-    with open(file_path, "r") as handle:
-        return json.load(handle)
+    return json.loads(load_file(file_path))
 
 
-animals_data = load_data('animals_data.json')
+def get_animals_text(animals_data):
+    animal_texts = []
+    for animal in animals_data:
+        text = ""
+        name = animal.get("name", None)
+        if name:
+            text += f"Name: {name}\n"
+        locations = animal.get("locations", None)
+        if locations:
+            text += f"Location: {locations[0]}\n"
+        characteristics = animal.get("characteristics", None)
+        if characteristics:
+            diet = characteristics.get("diet", None)
+            if diet:
+                text += f"Diet: {diet}\n"
+            animal_type = characteristics.get("type", None)
+            if animal_type:
+                text += f"Type: {animal_type}\n"
+        animal_texts.append(text)
+    return "\n".join(animal_texts)
 
-for animal in animals_data:
-    text = ""
-    name = animal.get("name", None)
-    if name:
-        text += "Name: " + name + "\n"
-    locations = animal.get("locations", None)
-    if locations:
-        text += "Location: " + locations[0] + "\n"
-    characteristics = animal.get("characteristics", None)
-    if characteristics:
-        diet = characteristics.get("diet", None)
-        if diet:
-            text += "Diet: " + diet + "\n"
-        animal_type = characteristics.get("type", None)
-        if animal_type:
-            text += "Type: " + animal_type + "\n"
-    text = text
-    print(text)
+
+def main():
+    animals_data = load_data("animals_data.json")
+
+    animals_text = get_animals_text(animals_data)
+
+    template_text = load_file("animals_template.html")
+
+    updated_template = template_text.replace(
+        "__REPLACE_ANIMALS_INFO__",
+        animals_text,
+    )
+
+    write_file("animals_template.html", updated_template)
+
+if __name__ == "__main__":
+    main()
